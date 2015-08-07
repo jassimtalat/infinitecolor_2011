9b1f827ba59171569ebd0a904003a606a7c925ab
Jassim Talat <jassim.talat@gmail.com>
2015-08-07 18:04:25 +0500

    commit
    commit

---
+++ contact-form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="css/reset.css" rel="stylesheet" type="text/css" />
+<link href="css/fonts.css" rel="stylesheet" type="text/css" />
+<link href="css/global.css" rel="stylesheet" type="text/css" media="screen" />
+<title></title>
+</head>
+
+<body>
+<form name="contactform" method="post" action="send_form_email.php">
+<input name="first_name" type="text" value="First Name" class="formfld" /><br />
+<input name="last_name" type="text" value="Last Name" class="formfld" /><br />
+<input name="phone" type="text" value="Phone" class="formfld" /><br />
+<input name="email" type="text" value="Email" class="formfld" /><br />
+<textarea name="comments" cols="" class="formarea">Comments</textarea><br />
+<input name="submit" type="submit" value="SUBMIT" class="submit-bt"/>
+</form>
+</body>
+</html>
